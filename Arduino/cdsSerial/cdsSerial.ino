3edb3cfc87d70eee6721967ce79c06c5e171e6d4
Brian Kim <brik0519@gmail.com>
2024-11-25 13:46:29 +0900

    Update to Final Code

--- Arduino/cdsSerial/cdsSerial.ino
+++ Arduino/cdsSerial/cdsSerial.ino
@@ -2,9 +2,10 @@
 
 #define LDR_PIN A0         // 조도 센서 핀
 #define LED_BUILTIN 13
-bool isCounting = false;        // LED가 켜져 있는 상태에서 40초 카운트 여부
+#define LED_DURATION = 40000;
 
-int LED_DURATION = 40000;
+bool isCounting = false;        // LED가 켜져 있는 상태에서 40초 카운트 여부
+int ldrValue = 0;
 
 void setup() {
   Serial.begin(9600);        // 시리얼 통신 시작
@@ -14,13 +15,14 @@ void setup() {
 void loop() {
   // 조도 센서 상태 확인
   checkLDR();
+  // Serial.println(ldrValue);
   delay(1500);
 }
 
 
 void checkLDR() {
-  int ldrValue = analogRead(LDR_PIN);
-  if (ldrValue > 900) {
+  ldrValue = analogRead(LDR_PIN);
+  if (ldrValue > 500/*900*/) {
     Serial.println("ON");
     digitalWrite(LED_BUILTIN, HIGH);
     isCounting = true;
